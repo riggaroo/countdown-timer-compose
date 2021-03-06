8cd5ee54f33bf11128cfcafd74539615960c7e21
Rebecca Franks <franks.rebecca@gmail.com>
2021-03-06 13:33:42 +0200

    Added animated gradient background

--- app/src/main/java/com/example/androiddevchallenge/timer/ui/Background.kt
+++ app/src/main/java/com/example/androiddevchallenge/timer/ui/Background.kt
@@ -1,20 +1,56 @@
-package com.example.androiddevchallenge
+package com.example.androiddevchallenge.timer.ui
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.drawscope.rotate
 import com.example.androiddevchallenge.ui.theme.lightPurple
 import com.example.androiddevchallenge.ui.theme.orange
 import com.example.androiddevchallenge.ui.theme.peach
 
 @Composable
 fun BackgroundGradient(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val colorFirst by infiniteTransition.animateColor(
+        initialValue = orange,
+        targetValue = peach,
+        animationSpec = infiniteRepeatable(
+            animation = tween(3000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val colorSecond by infiniteTransition.animateColor(
+        initialValue = peach,
+        targetValue = lightPurple,
+        animationSpec = infiniteRepeatable(
+            animation = tween(3000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val colorThird by infiniteTransition.animateColor(
+        initialValue = lightPurple,
+        targetValue = orange,
+        animationSpec = infiniteRepeatable(
+            animation = tween(3000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
     Canvas(modifier = modifier.fillMaxSize()) {
         val brushBackground = Brush.verticalGradient(
-            listOf(orange, peach, lightPurple), 0f, size.height.toDp().toPx(), TileMode.Mirror
+            listOf(colorFirst, colorSecond, colorThird), 0f, size.height.toDp().toPx(), TileMode.Mirror
         )
         drawRect(brushBackground)
     }
--- app/src/main/java/com/example/androiddevchallenge/timer/ui/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/timer/ui/TimerScreen.kt
@@ -6,7 +6,6 @@ import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
@@ -18,11 +17,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonElevation
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -34,7 +31,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.androiddevchallenge.BackgroundGradient
+import com.example.androiddevchallenge.timer.ui.BackgroundGradient
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.timer.ext.formatDuration
 import kotlinx.coroutines.flow.collect
@@ -64,7 +61,8 @@ fun TimerScreen(timerViewModel: CountdownTimerViewModel, navController: NavContr
     }
     Surface(color = MaterialTheme.colors.background) {
         BackgroundGradient()
-        Column(modifier = Modifier.padding(16.dp)) {
+        Column(modifier = Modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center) {
             when (state.timerViewState) {
                 TimerViewState.RUNNING -> {
                     TimerText(timerText = state.durationRemaining.formatDuration())
@@ -133,7 +131,7 @@ fun TimerText(modifier: Modifier = Modifier, timerText: String) {
     Text(modifier = modifier
         .fillMaxWidth()
         .padding(16.dp),
-        style = MaterialTheme.typography.h2,
+        style = MaterialTheme.typography.h1,
         text = timerText,
         textAlign = TextAlign.Center
     )
